ceb30ebda511a5b2df2efde396ab53531ce88f71
Mohan Lal <128143049+mhnuk2007@users.noreply.github.com>
2026-01-21 19:33:52 +0000

    submit solution to challege 03_05

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,7 +1,14 @@
 package com.mycompany.app;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
   public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("artclass_persistence_unit");
+    EntityManager em = emf.createEntityManager();
+
 
   }
 }
